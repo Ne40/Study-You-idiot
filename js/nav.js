0c15380cb17a9c2a593d9a6ca04c2374b2c2ad20
Nero <nerovirusismail@gmail.com>
2026-04-25 16:14:59 +0300

    applay to all bages

--- js/nav.js
+++ js/nav.js
@@ -1,23 +1,26 @@
+const navMenu = document.getElementById("navMenu");
 const userBox = document.getElementById("userBox");
 
 let user = localStorage.getItem("user");
 
 if (user) {
-    userBox.innerHTML = `
-        <div class="user-info">
-            <li><a href="intro.html">Home</a></li>
-            <li><a href="rooms.html">Rooms</a></li>
-            <li class="welcome">Welcome, ${user.split("@")[0]} 👋</li>
-            <li><a href="#" onclick="logout()">Logout</a></li>
-        </div>
+    navMenu.innerHTML = `
+        <li><a href="intro.html">Home</a></li>
+        <li><a href="rooms.html">Rooms</a></li>
+        <li class="welcome">Welcome, ${user.split("@")[0]} 👋</li>
+        <li><a href="#" onclick="logout()">Logout</a></li>
     `;
+
+    userBox.innerHTML = "";
 } else {
-    userBox.innerHTML = `
+    navMenu.innerHTML = `
         <li><a href="intro.html">Home</a></li>
         <li><a href="rooms.html">Rooms</a></li>
         <li><a href="Log-in.html">Log-in</a></li>
         <li><a href="Sign-up.html">Sign-up</a></li>
     `;
+
+    userBox.innerHTML = "";
 }
 
 function logout() {
